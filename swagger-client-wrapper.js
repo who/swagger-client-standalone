52a83c6d8ded545dab3d1b2712957ddc13e975ed
Andrew B <who@users.noreply.github.com>
2015-08-17 12:57:17 -0700

    Updating bin command, restructuring folders

--- swagger-client-wrapper.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(options){
-    var SwaggerClient = require('swagger-client');
-    var theSwag = require('swagger-spec-file-alias');
-	options = options || {};
-	options.spec = theSwag;
-	return new SwaggerClient(options);
-};
